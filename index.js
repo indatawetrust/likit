7a4fd2fd5ff7de2f1577f526e168813a6ce81ee4
Ahmet Simsek <iamaroott@gmail.com>
2017-05-23 01:28:49 +0300

    .

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 module.exports = {
   koa: require('./lib/koa1'),
   koa2: require('./lib/koa2'),
   express: require('./lib/express'),
 }
-=======
-module.exports = require('./lib/likit');
->>>>>>> master
